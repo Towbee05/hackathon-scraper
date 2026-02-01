54822b8f47d3a54431e8ae28aa6d875fdd714d2a
Towbee05 <olatiseoluwatobiloba@gmail.com>
2026-02-01 18:09:20 +0100

    Added selenium as web scraper

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import time
+
+URL = "https://devpost.com/hackathons?pages=5"
+def fetch_data(url):
+    try:
+        # initializing selenium chrome service
+        service = Service(ChromeDriverManager().install())
+
+        # initializing browser (chrome)
+        browser = webdriver.Chrome(service=service)
+        
+        # Load dynamic page
+        browser.get(url)
+        # Explicitly wait for dynamic page to load (hackathons container)
+        WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "hackathon-tile")))
+
+        
+        # print(soup)
+    except Exception as error:
+        # Exception to catch errors
+        print(error)
+        raise error
+
+fetch_data(URL)
