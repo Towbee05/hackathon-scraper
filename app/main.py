9db010f0e9f65b025552889431c662fa25aec518
Towbee05 <olatiseoluwatobiloba@gmail.com>
2026-02-01 18:53:43 +0100

    Finished hackathon scraper

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 
-URL = "https://devpost.com/hackathons?pages=5"
+URL = "https://devpost.com/hackathons?pages=6"
 def fetch_data(url):
     try:
         # initializing selenium chrome service
@@ -31,10 +31,29 @@ def fetch_data(url):
         hackathons = soup.select('.hackathon-tile')
         print(len(hackathons))
         for hackathon in hackathons:
+            image_link = hackathon.select_one('img')
             title = hackathon.select_one('h3')
-            print(title)
+            hackathon_link = hackathon.select_one('a.tile-anchor')
+            prize = hackathon.select_one('span.prize-amount')
+            participants = hackathon.select_one('div.participants')
+            days_left = hackathon.select_one('.status-label')
+            submission_period = hackathon.select_one('.submission-period')
+            organization = hackathon.select_one('.host-label')
+            themes = hackathon.select('.theme-label')
 
-        # print(soup)
+            data = {
+                "img" : image_link['src'] if image_link else None,
+                "title" : title.get_text(strip=True) if title else None,
+                "link" : hackathon_link['href'] if hackathon_link else None,
+                "organization" : organization.get_text(strip=True) if organization else None,
+                "prize" : prize.get_text(strip=True) if prize else None,
+                "participants" : participants.get_text(strip=True) if participants else None,
+                "submission period" : submission_period.get_text(strip=True) if submission_period else None,
+                "days left" : days_left.get_text(strip=True) if days_left else None,
+                "themes" : [theme.get_text(strip=True) for theme in themes] if themes else []
+            }
+            fetched_hackathons.append(data)
+        print(fetched_hackathons)
     except Exception as error:
         # Exception to catch errors
         print(error)
